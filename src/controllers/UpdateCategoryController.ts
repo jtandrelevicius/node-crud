c1bc8fb663efb34871aa5bdcad3eb7721885df20
Jefferson Tiago <jtandrelevicius@gmail.com>
2022-03-26 14:38:48 -0400

    update files

---
+++ src/controllers/UpdateCategoryController.ts
@@ -0,0 +1,21 @@
+import { Request, response, Response } from "express";
+import { UpdateCategoryService } from "../services/UpdateCategoryService";
+
+
+export class UpdateCategoryController {
+    async handle(req: Request, res: Response){
+        const { id } = req.params
+        const { name, description } = req.body
+
+        const service = new UpdateCategoryService();
+
+        const result = await service.execute({id , name , description});
+
+        if (result instanceof Error){
+            return response.status(400).json(result.message);
+        }
+
+        return res.json(result);
+
+    }
+}
