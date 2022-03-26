2dd5b1b4a6e1f42ce806ea2eb973f3c95763efa4
Jefferson Tiago <jtandrelevicius@gmail.com>
2022-03-26 11:08:29 -0400

    create CreateCategoryController

--- src/controllers/CreateCategoryController.ts
+++ src/controllers/CreateCategoryController.ts
@@ -1,7 +1,15 @@
-
-
+import { Request, Response } from "express";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 export class CreateCategoryController{
     async handle(req: Request, res: Response){
-        
+        const { name, description } = req.body();
+
+        const service = new CreateCategoryService();
+
+        const result = await service.execute({name, description});
+
+        if (result instanceof Error) {
+            return res.status(400).json(result.message);
+        }
     }
 }
